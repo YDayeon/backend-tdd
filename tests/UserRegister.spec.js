fac53b6ecb6cbaccd3123fea0282d88901bb582b
Dayeon Yoon <yday1223@gmail.com>
2024-05-02 10:02:51 +0900

    setting configration

---
+++ tests/UserRegister.spec.js
@@ -0,0 +1,50 @@
+const request = require('supertest');
+const app = require('../src/app');
+const User = require('../src/user/User');
+
+describe('User Registration', () => {
+  it('return 200 OK when signup request is valid', (done) => {
+    request(app)
+      .post('/api/1.0/users')
+      .send({
+        username: 'user1',
+        email: 'user1@mail.com',
+        password: 'P4ssword',
+      })
+      .then((response) => {
+        expect(response.status).toBe(200);
+        done();
+      });
+  });
+
+  it('return success message when request is valid', (done) => {
+    request(app)
+      .post('/api/1.0/users')
+      .send({
+        username: 'user1',
+        email: 'user1@mail.com',
+        password: 'P4ssword',
+      })
+      .then((response) => {
+        expect(response.body.message).toBe('User created');
+        done();
+      });
+  });
+
+  it('saves the user to database', (done) => {
+    request(app)
+      .post('/api/1.0/users')
+      .send({
+        username: 'user1',
+        email: 'user1@mail.com',
+        password: 'P4ssword',
+      })
+      .then(() => {
+        // query user table
+        User.findAll().then((userList) => {
+          expect(userList.length).toBe(1);
+          done();
+        });
+      });
+  });
+});
