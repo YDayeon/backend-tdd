f95717524712930728bcb214c78f05b542fe83b6
Dayeon Yoon <yday1223@gmail.com>
2024-05-07 14:39:23 +0900

    feat: validation

--- src/user/UserRouter.js
+++ src/user/UserRouter.js
@@ -1,12 +1,51 @@
 const express = require('express');
 const UserService = require('./UserService');
+const { check, validationResult } = require('express-validator');
 
 const router = express.Router();
 
-router.post('/api/1.0/users', async (req, res) => {
-  await UserService.save(req.body);
+router.post(
+  '/api/1.0/users',
+  check('username')
+    .notEmpty()
+    .withMessage('Username cannot be null')
+    .bail()
+    .isLength({ min: 4, max: 32 })
+    .withMessage('Must have min 4 and max 32 characters'),
+  check('email')
+    .notEmpty()
+    .withMessage('E-mail cannot be null')
+    .bail()
+    .isEmail()
+    .withMessage('E-mail is not valid')
+    .bail()
+    .custom(async (email) => {
+      const user = await UserService.findByEmail(email);
+      if (user) {
+        throw new Error('E-mail in use');
+      }
+    }),
+  check('password')
+    .notEmpty()
+    .withMessage('Password cannot be null')
+    .bail()
+    .isLength({ min: 6 })
+    .withMessage('Password must be at least 6 characters')
+    .bail()
+    .matches(/^(?=.*[a-z])(?=.*[A-Z])(?=.*\d).*$/)
+    .withMessage('Password must have at least 1 uppercase, 1 lowercase and 1 number'),
+  async (req, res) => {
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+      const validationErrors = {};
+      errors.array().forEach((error) => (validationErrors[error.param] = error.msg));
+      return res.status(400).send({ validationErrors: validationErrors });
+    }
 
-  return res.send({ message: 'User created' });
-});
+    await UserService.save(req.body);
+
+    return res.send({ message: 'User created' });
+  }
+);
 
 module.exports = router;
